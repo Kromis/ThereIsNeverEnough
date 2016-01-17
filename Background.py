bb80e740fc7b7104138e85f682174fd0634bce47
Andrew <aschau@uci.edu>
2016-01-16 23:14:21 -0800

    Fixed sun and health decreasing. Bug with negative health

--- Background.py
+++ Background.py
@@ -21,7 +21,7 @@ class Background:
         self.status_green = resources.all_sprites["statusGreen.png"]
         self.status_red = resources.all_sprites["statusRed.png"]
         self.sky = self.sun
-        self.sky_position = [resources.width/2, resources.height/2]
+        self.sky_position = (resources.width/3, 0)
         
         self.font_size = 100
         self.font = pygame.font.Font(pygame.font.match_font('cooperblack'), self.font_size)
@@ -56,7 +56,6 @@ class Background:
         else:
             self.sky = self.moon
 
-        self.sky_position[1] += 10
         self.time_text = '{:02d}:{:02d}'.format(resources.game_manager.time.hour, resources.game_manager.time.minute)
 
         for i in range(len(self.cloud_list[0])):
@@ -68,17 +67,16 @@ class Background:
 
     def draw(self):
         self.screen.blit(self.background, (0, 0))
-        self.screen.blit(self.sky, (self.sky_position[0], self.sky_position[1]))
         for i in range(0, len(self.cloud_list[0])):
             self.screen.blit(resources.all_sprites[self.cloud_list[0][i][3]], (self.cloud_list[0][i][0], self.cloud_list[0][i][1]))
         self.screen.blit(self.ship, (50, 200))
-
+        self.screen.blit(self.sky, self.sky_position)
         self.screen.blit(self.font.render(self.time_text, True, pygame.Color(255, 255, 255)), (120, 0))
         for item in range(len(self.sidebar)):
             self.screen.blit(self.sidebar[item], (10, 100*item))
             
         self.screen.blit(self.status_red, (110, 50 + 100))
-        self.screen.blit(self.status_green, (110, 50 + 100), (0, 0, resources.game_manager.ship_health*2, 20))
+        self.screen.blit(self.status_green, (110, 50 + 100), (0, 0, resources.game_manager.shipHp*2, 20))
 
         self.screen.blit(self.status_red, (110, 50 + 200))
         self.screen.blit(self.status_green, (110, 50 + 200), (0, 0, resources.game_manager.ship_power*2, 20))
