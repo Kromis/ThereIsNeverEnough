c98b9e7d4e3618b9a78e555c2664de1bedfc10fe
Andrew <aschau@uci.edu>
2016-01-16 16:36:25 -0800

    Added get_corners to Draw_Comp.py and key_press event checking to Going Home.py

--- Going Home.py
+++ Going Home.py
@@ -1,8 +1,10 @@
 import os, sys
 import pygame
 import resources
+import Game
 from Background import Background
 from Draw_Comp import Draw_Comp
+
 
 from pygame.locals import *
 
@@ -17,7 +19,7 @@ class GoingHome:
             image.convert_alpha()
             
         self.background = Background(self.screen)
-
+        self.game = Game.Game()
         self.gameState = "GAME"
     
     def game_loop(self):
@@ -28,12 +30,15 @@ class GoingHome:
                 for event in pygame.event.get():
                     if (event.type == pygame.QUIT):
                         resources.main_loop_running = False
+
+                    if pygame.mouse.get_pressed()[0]:
+                        self.game.click(pygame.mouse.get_pos())
+                        print(str(pygame.mouse.get_pos()[0]) + ", " + str(pygame.mouse.get_pos()[1]))                    
                 
                 self.background.cloud_update()
                 self.background.draw()
-                Draw_Comp("cannon.png", self.screen, (200, 500)).draw(True, False
-)
-
+                test = Draw_Comp("shield.png", self.screen, (200, 500))
+                test.draw(False, False)
 
             pygame.display.update()
         pygame.quit()
